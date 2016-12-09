3873d038a02f499403d1a506bd039ef1d3fb0b9c
Daniel Lundin <daniel.lundin@scilifelab.se>
2016-12-09 16:45:54 +0100

    ...

--- testapp/ui.R
+++ testapp/ui.R
@@ -10,35 +10,36 @@
 library(shiny)
 
 # Define UI for application that draws a histogram
-shinyUI(fluidPage(
-  
-  # Application title
-  titlePanel("Pfitmap testapp"),
-  
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-      selectInput(
-        'taxon_rank', 'Taxon rank', 
-      	c(
-      	  'Domain' = 'tdomain', 'Kingdom' = 'tkingdom', 'Phylum' = 'tphylum',
-      	  'Class' = 'tclass', 'Order' = 'torder', 'Family' = 'tfamily',
-      	  'Genus' = 'tgenus', 'Species' = 'tspecies', 'Strain' = 'tstrain'
-      	)
-      ),
-      selectInput(
-        'protein_rank', 'Protein rank', 
-      	c(
-      	  'Superfamily' = 'psuperfamily', 'Family' = 'pfamily', 
-      	  'Class' = 'pclass', 'Subclass' = 'psubclass', 'Group' = 'pgroup'
-      	)
-      )
-    ),
+shinyUI(
+  fluidPage(
+    # Application title
+    titlePanel("Pfitmap testapp"),
     
-    # Show a plot of the generated distribution
-    mainPanel(
-      tableOutput("matrix"),
-      plotOutput("summaryplot")
+    # Sidebar with a slider input for number of bins 
+    sidebarLayout(
+      sidebarPanel(
+	selectInput(
+	  'taxon_rank', 'Taxon rank', 
+	  c(
+	    'Domain' = 'tdomain', 'Kingdom' = 'tkingdom', 'Phylum' = 'tphylum',
+	    'Class' = 'tclass', 'Order' = 'torder', 'Family' = 'tfamily',
+	    'Genus' = 'tgenus', 'Species' = 'tspecies', 'Strain' = 'tstrain'
+	  )
+	),
+	selectInput(
+	  'protein_rank', 'Protein rank', 
+	  c(
+	    'Superfamily' = 'psuperfamily', 'Family' = 'pfamily', 
+	    'Class' = 'pclass', 'Subclass' = 'psubclass', 'Group' = 'pgroup'
+	  )
+	)
+      ),
+      
+      # Show a plot of the generated distribution
+      mainPanel(
+	tableOutput("matrix"),
+	plotOutput("summaryplot")
+      )
     )
   )
-))
+)
