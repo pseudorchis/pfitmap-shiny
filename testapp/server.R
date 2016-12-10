98cd35c60265a97da0e9251909801a021caf7a41
Daniel Lundin <daniel.lundin@scilifelab.se>
2016-12-10 09:53:19 +0100

    Subsetting by psuperfamily finally works. It was ifelse() that didn't return the whole vector it seems.

--- testapp/server.R
+++ testapp/server.R
@@ -12,18 +12,16 @@ library(ggplot2)
 
 # Constants
 
-
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-  dataInput <- reactive({
-    write(cat(c('input$psuperfamilies:', input$psuperfamilies)), stderr())
+  dataMatrix <- reactive({
     data <- genomes %>%
       group_by_(.dots = c(as.symbol(input$taxon_rank))) %>%
       summarise(ngenomes = n()) %>%
       ungroup() %>%
       inner_join(
         cp %>%
-          filter(psuperfamily %in% ifelse(length(input$psuperfamilies) == 0, psuperfamilies$psuperfamily, input$psuperfamilies)) %>%
+          filter(psuperfamily %in% dataPSuperfamiliesFilter()) %>%
           group_by_(.dots = lapply(c(input$taxon_rank, input$protein_rank), as.symbol)) %>%
           summarise(nproteins=n()) %>%
           ungroup(),
@@ -31,15 +29,22 @@ shinyServer(function(input, output) {
       ) %>%
       mutate_('taxon' = input$taxon_rank, 'protein' = input$protein_rank)
   })
+  
+  dataPSuperfamiliesFilter <- reactive({
+    if ( length(input$psuperfamilies) == 0 ) {
+      return(psuperfamilies$psuperfamily)
+    }
+    input$psuperfamilies
+  })
 
   output$matrix <- renderTable({
-    dataInput() %>%
+    dataMatrix() %>%
       select(taxon, protein, ngenomes, nproteins) %>%
       spread(protein, nproteins)
   })
   
   output$summaryplot <- renderPlot({
-    ggplot(dataInput(), aes(x=taxon, y=nproteins, colour=protein)) +
+    ggplot(dataMatrix(), aes(x=taxon, y=nproteins, colour=protein)) +
       geom_point() +
       theme(
         axis.text.x = element_text(angle=60, hjust=1)
