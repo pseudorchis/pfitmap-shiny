e517d86cc36a6db2c8ef6afdc25efc478316927c
Erik Danielsson <sthlmjerka@gmail.com>
2016-12-28 16:13:47 +0100

    Test application with DataTable rendering.

--- src/shiny/test/app.R
+++ src/shiny/test/app.R
@@ -8,43 +8,68 @@
 #
 
 library(shiny)
+library(readr)
+library(dplyr)
+library(tidyr)
+
+classified_proteins = read_tsv(
+  '../../../data/test/classified_proteins.10000.tsv',
+  col_types = cols(
+    .default = col_character(),
+    profile_length = col_integer(),
+    align_length = col_integer(),
+    align_start = col_integer(),
+    align_end = col_integer(),
+    prop_matching = col_double(),
+    ss_version = col_integer(),
+    e_value = col_double(),
+    score = col_double(),
+    profile_version = col_double()
+  )
+)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
+  # Application title
+  titlePanel("Eriks och Daniels testapp"),
    
-   # Application title
-   titlePanel("Old Faithful Geyser Data"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
-      ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
+  fluidRow(
+    column(2),
+    column(4, 
+      selectInput(
+        'taxonrank', 'Taxon rank', 
+        list(
+          'Domain'  = 'tdomain',  'Phylum'  = 'tphylum', 'Class'   = 'tclass',
+          'Order'   = 'torder',   'Family'  = 'tfamily', 'Genus'   = 'tgenus',
+          'Species' = 'tspecies', 'Strain'  = 'tstrain'
+        )
       )
-   )
+    ),
+    column(4,
+      selectInput(
+        'proteinrank', 'Protein rank',
+        list(
+          'Superfamily' = 'psuperfamily', 'Family'   = 'pfamily',
+          'Class'       = 'pclass',       'Subclass' = 'psubclass',
+          'Group'       = 'pgroup'
+        )
+      )
+    )
+  ),
+  
+  fluidRow(
+    column(12, dataTableOutput('mainmatrix'))
+  )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-   
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   })
+  output$mainmatrix = renderDataTable(
+    classified_proteins %>% group_by_(input$taxonrank, input$proteinrank) %>%
+      summarise(n=n())  %>% 
+      spread_(input$proteinrank, 'n', fill=0)
+  )
 }
 
 # Run the application 
 shinyApp(ui = ui, server = server)
-
