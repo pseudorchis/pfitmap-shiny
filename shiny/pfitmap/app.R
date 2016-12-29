1692ce97b6d93941119a15a8851d3e753d53c936
Erik Danielsson <sthlmjerka@gmail.com>
2016-12-29 17:27:49 +0100

    Adapted shiny test app to skeleton for pfitmap.

--- shiny/pfitmap/app.R
+++ shiny/pfitmap/app.R
@@ -10,28 +10,30 @@
 library(shiny)
 library(readr)
 library(dplyr)
+library(data.table)
+library(dtplyr)
 library(tidyr)
 
-classified_proteins = read_tsv(
-  '../../../data/test/classified_proteins.10000.tsv',
-  col_types = cols(
-    .default = col_character(),
-    profile_length = col_integer(),
-    align_length = col_integer(),
-    align_start = col_integer(),
-    align_end = col_integer(),
-    prop_matching = col_double(),
-    ss_version = col_integer(),
-    e_value = col_double(),
-    score = col_double(),
-    profile_version = col_double()
+classified_proteins = data.table(
+  read_tsv(
+    Sys.getenv('PFITMAP_DATA'),
+    col_types = cols(
+      .default = col_character(),
+      profile_length = col_integer(),
+      align_length = col_integer(),
+      align_start = col_integer(),
+      align_end = col_integer(),
+      prop_matching = col_double(),
+      ss_version = col_integer(),
+      e_value = col_double(),
+      score = col_double(),
+      profile_version = col_double()
+    )
   )
 )
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-  # Application title
-  titlePanel("Eriks och Daniels testapp"),
    
   fluidRow(
     column(2),
